7b56e5faeb48dbe4f86e06b9eb2f2cfb5d46335e
A.J. Ianozi <aj@ianozi.com>
2025-06-08 03:37:45 -0400

    Added support for VSS and broke out file loading.

--- readme.md
+++ readme.md
@@ -1,8 +1,29 @@
 # JSON benchmark for Ada
 
-This is just a simple json benchmark utilizing [json-ada](https://github.com/onox/json-ada) and [GNATCOLL.JSON](https://github.com/adacore/gnatcoll-core).  VSS.json is also included in the alire project, but not used.
+This is just a simple json benchmark utilizing [json-ada](https://github.com/onox/json-ada), [GNATCOLL.JSON](https://github.com/adacore/gnatcoll-core), and [VSS.JSON](https://github.com/AdaCore/VSS).
 
 The large json file was from [json-iterator/test-data](https://github.com/json-iterator/test-data). I edited the file, multiplied it by about 16, and added named arrays.
+
+All it does is find the array with the key "sixteen", and then does this:
+```ada
+declare
+   Random_Node : constant JSON_Array := Result.Get ("sixteen");
+begin
+   for X of Random_Node loop
+      declare
+         Random_String : constant String := X.Get ("id");
+         Random_Int    : constant Long_Integer := X.Get ("actor").Get ("id");
+         Another_Int : Long_Integer;
+      begin
+         Another_Int := Random_Int + 1;
+      end;
+   end loop;
+end;
+```
+
+All of the JSON data is pre-loaded as a string or stream before any timers are ran, so this is just testing the actual processing.
+
+It times how long it takes to parse / load the JSON data, how long it takes to get to the "sixteen" key, and how long it takes to iterate the object.
 
 ## How to use it
 
@@ -12,10 +33,27 @@ After that, you must **unzip** the `large-file.zip` file to `large-file.json`.
 
 To run, issue the following command from the project directory: `bin/json_benchmark large-file.json`
 
+## My results
+
+This was the results on my computer, which is an M2 MacBook Air with 16GB of ram.
+
+```
+Testing json-ada's JSON
+json-ada read is  2.274622000 seconds
+json-ada seek is  0.001402000 seconds
+json-ada iter is  0.008116000 seconds
+Testing GNATCOLL's JSON
+GNATCOLL.JSON read is  5.622553000 seconds
+GNATCOLL.JSON seek is  0.005264000 seconds
+GNATCOLL.JSON iter is  0.004949000 seconds
+Testing VSS's JSON
+VSS.JSON read is  11.717198000 seconds
+VSS.JSON seek is  31.041688000 seconds
+VSS.JSON iter is  2.125321000 seconds
+```
+
+json-ada and GNATCOLL.JSON both use standard dictionary lookups, but VSS's json system is loads things into a stream that you have to manually parse afterwards, so it's more complicated, and there's probably a better way to do that section.  If someone wants to try and get the VSS.JSON one faster, please let me know!
+
 ## Contribute
 
-If you would like to contribute, feel free to add VSS to this.  I already have the following in the `alire.toml` file:
-```
-[[depends-on]]
-vss = "^23.0.0"
-```
+If you would like to contribute, feel free.  Maybe you can get VSS to speed up with the sample data...
--- src/json_benchmark.adb
+++ src/json_benchmark.adb
@@ -12,12 +12,28 @@
 --
 --    You should have received a copy of the GNU General Public License
 --    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma Ada_2022;
+with Ada.Text_IO;                use Ada.Text_IO;
+with Ada.Calendar;               use Ada.Calendar;
+with Ada.Command_Line;           use Ada.Command_Line;
+with Ada.Strings.Unbounded;      use Ada.Strings.Unbounded;
+with Ada.Characters.Latin_1;     use Ada.Characters.Latin_1;
+with Ada.Characters.Conversions; use Ada.Characters.Conversions;
 
-with Ada.Text_IO;            use Ada.Text_IO;
-with Ada.Calendar;           use Ada.Calendar;
-with Ada.Command_Line;       use Ada.Command_Line;
-with Ada.Strings.Unbounded;  use Ada.Strings.Unbounded;
-with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
+with JSON.Streams;
+
+with VSS.Strings;
+with VSS.JSON;
+with VSS.JSON.Streams;
+with VSS.JSON.Pull_Readers;
+with VSS.JSON.Pull_Readers.Simple;
+with VSS.Stream_Element_Vectors;
+with VSS.Strings.Conversions;
+with VSS.Strings.Converters;
+with VSS.Strings.Converters.Encoders;
+--with VSS.String_Vectors;
+with VSS.Text_Streams.Memory_UTF8_Input;
+--with VSS.Stream_Element_Vectors.Conversions;
 
 with GNATCOLL.JSON;
 with JSON.Parsers;
@@ -26,15 +42,44 @@ with JSON.Types;
 procedure Json_Benchmark is
 
    Invalid_Args : exception;
+   Invalid_File : exception;
+
+   --  The special key and values in the large-file.json file
+   True_Random_Key     : constant String       := "Sixteen";
+   True_Random_Key_VSS : constant VSS.Strings.Virtual_String := "Sixteen";
 
    --  Our json File Name.
-   Json_Path : constant String :=
+   JSON_Path : constant String :=
               (if Argument_Count = 1 then Argument (1) else
                raise Invalid_Args with
                   "Usage: ./json_benchmark ""file.json""");
-   F : File_Type;
 
-   Contents : Unbounded_String := Null_Unbounded_String;
+   --  To read in the json file in.
+   --  I don't know how to read gnatcoll.json or vss from a file :)
+   --  But I will not penalize gnatcoll.json for this.
+   function Read_File (File_Path : String) return String is
+      Result : Unbounded_String := Null_Unbounded_String;
+      F : File_Type;
+   begin
+      Open (F, In_File, File_Path);
+      while not End_Of_File (F) loop
+         Append (Result, Get_Line (F));
+         Append (Result, CR & LF);
+      end loop;
+      Close (F);
+      return To_String (Result);
+   end Read_File;
+   --  For gnatcoll
+   JSON_String : constant String := Read_File (JSON_Path);
+
+   --  for VSS
+   JSON_VSS    : constant VSS.Strings.Virtual_String :=
+                     VSS.Strings.To_Virtual_String
+                        (To_Wide_Wide_String (JSON_String));
+
+   --  For json_ada (I cannot just pass the string due to stack overflow)
+   JSON_Stream : constant JSON.Streams.Stream_Element_Array_Access :=
+                           JSON.Streams.From_File (JSON_Path);
 
    --  Timings
    Start_Time  : Time;
@@ -42,16 +87,6 @@ procedure Json_Benchmark is
    Seconds : Duration;
 
 begin
-
-   --  Read in the json file in.
-   --  I don't know how to read gnatcoll.json from a file :)
-   --  But I will not penalize gnatcoll.json for this.
-   Open (F, In_File, Json_Path);
-   while not End_Of_File (F) loop
-      Append (Contents, Get_Line (F));
-      Append (Contents, CR & LF);
-   end loop;
-   Close (F);
 
    --  json-ada
    Put_Line ("Testing json-ada's JSON");
@@ -62,23 +97,30 @@ begin
          (Long_Integer, Long_Float, Maximum_Number_Length => 1000);
       package Parsers is new JSON.Parsers
          (Types, Default_Maximum_Depth => 100000);
-      Parser : Parsers.Parser := Parsers.Create_From_File (Json_Path);
+      --  I can only either read from a file or use a stream element array
+      --  Otherwise I get a stackoverflow with JSON_Ada
+      Parser : Parsers.Parser := Parsers.Create (JSON_Stream);
       Result : constant Types.JSON_Value := Parser.Parse;
-      use Parsers; use Types;
+      use Types;
    begin
       End_Time := Clock;
       Seconds := (End_Time - Start_Time);
       Put_Line ("json-ada read is " & Seconds'Image & " seconds");
-      --  Iterate
+      --  Seek
       Start_Time := Clock;
       declare
-         Random_Node : constant JSON_Value := Result.Get ("Sixteen");
+         Random_Node : constant JSON_Value := Result.Get (True_Random_Key);
       begin
+         End_Time := Clock;
+         Seconds := (End_Time - Start_Time);
+         Put_Line ("json-ada seek is " & Seconds'Image & " seconds");
+         -- Iterate
+         Start_Time := Clock;
          for X of Random_Node loop
             declare
                Random_String : constant String := X.Get ("id").Value;
                Random_Int    : constant Long_Integer :=
-                                          X.Get ("actor").Get ("id").Value;
+                                 X.Get ("actor").Get ("id").Value;
                Another_Int : Long_Integer;
             begin
                Another_Int := Random_Int + 1;
@@ -86,7 +128,7 @@ begin
          end loop;
          End_Time := Clock;
          Seconds := (End_Time - Start_Time);
-         Put_Line ("json-ada iterate is " & Seconds'Image & " seconds");
+         Put_Line ("json-ada iter is " & Seconds'Image & " seconds");
       end;
    end Json_Ada_Test;
 
@@ -96,7 +138,7 @@ begin
    GNATCOLL_Test :
    declare
       use GNATCOLL.JSON;
-      Result : constant JSON_Value := Read (Contents);
+      Result : constant JSON_Value := Read (JSON_String);
    begin
       End_Time := Clock;
       Seconds := (End_Time - Start_Time);
@@ -104,24 +146,209 @@ begin
       --  Iterate
       Start_Time := Clock;
       declare
-         Random_Node : constant JSON_Array := Result.Get ("Sixteen");
+         Random_Node : constant JSON_Array := Result.Get (True_Random_Key);
       begin
+         --  Seek
+         End_Time := Clock;
+         Seconds := (End_Time - Start_Time);
+         Put_Line ("GNATCOLL.JSON seek is " & Seconds'Image & " seconds");
+         -- Iterate
+         Start_Time := Clock;
          for X of Random_Node loop
             declare
                Random_String : constant String := X.Get ("id");
-               Random_Int    : constant Integer := X.Get ("actor").Get ("id");
-               Another_Int : Integer;
+               Random_Int    : constant Long_Integer :=
+                                 X.Get ("actor").Get ("id");
+               Another_Int : Long_Integer;
             begin
                Another_Int := Random_Int + 1;
             end;
          end loop;
          End_Time := Clock;
          Seconds := (End_Time - Start_Time);
-         Put_Line ("GNATCOLL.JSON iterate is " & Seconds'Image & " seconds");
+         Put_Line ("GNATCOLL.JSON iter is " & Seconds'Image & " seconds");
       end;
    end GNATCOLL_Test;
 
-   --  You can add VSS Here if you want, I don't know how to do the above
-   --  with VSS.json ^
+   --  VSS
+   --  This one was complicated because VSS's json doesn't seem to just load
+   --  everything into a dictionary.  It's much more low level. PLEASE let me
+   --  know if VSS can just load things into a dictionary out-of-the-box :)
+   Put_Line ("Testing VSS's JSON");
+   Start_Time := Clock;
+   VSS_Test :
+   declare
+      use VSS.Strings;
+      use VSS.Strings.Converters;
+      use VSS.Strings.Conversions;
+      use VSS.Stream_Element_Vectors;
+      use VSS.Strings.Converters.Encoders;
+      use type VSS.JSON.Streams.JSON_Stream_Element_Kind;
+
+      type Random_Object is record
+         Random_String : VSS.Strings.Virtual_String;
+         Random_Int    : Long_Integer;
+      end record;
+
+      procedure Skip_Object
+         (Reader  : in out VSS.JSON.Pull_Readers.JSON_Pull_Reader'Class)
+      is
+      begin
+         loop
+            case Reader.Read_Next is
+               when VSS.JSON.Streams.Start_Object =>
+                  Skip_Object (Reader);
+               when VSS.JSON.Streams.End_Object |
+                    VSS.JSON.Streams.End_Document =>
+                  exit;
+
+               when others =>
+                  null;
+            end case;
+         end loop;
+      end Skip_Object;
+
+      procedure Skip_Array
+         (Reader  : in out VSS.JSON.Pull_Readers.JSON_Pull_Reader'Class)
+      is
+      begin
+         loop
+            case Reader.Read_Next is
+               when VSS.JSON.Streams.Start_Array =>
+                  Skip_Array (Reader);
+               when VSS.JSON.Streams.End_Array |
+                    VSS.JSON.Streams.End_Document =>
+                  exit;
+
+               when others =>
+                  null;
+            end case;
+         end loop;
+      end Skip_Array;
+
+      procedure Read_Actor
+         (Reader  : in out VSS.JSON.Pull_Readers.JSON_Pull_Reader'Class;
+          Item    : out Long_Integer) is
+      begin
+         loop
+            case Reader.Read_Next is
+               when VSS.JSON.Streams.Key_Name =>
+                  if Reader.Key_Name = "id" then
+                     case Reader.Read_Next is
+                        when VSS.JSON.Streams.Number_Value =>
+                           Item := Long_Integer
+                              (VSS.JSON.As_Integer (Reader.Number_Value));
+                        when others =>
+                           raise Invalid_File;
+                     end case;
+                  end if;
+               when VSS.JSON.Streams.End_Object =>
+                  exit;
+               when others =>
+                  null;
+            end case;
+         end loop;
+      end Read_Actor;
+
+      procedure Read_Object
+         (Reader  : in out VSS.JSON.Pull_Readers.JSON_Pull_Reader'Class;
+          Item    : out Random_Object) is
+      begin
+         loop
+            case Reader.Read_Next is
+               when VSS.JSON.Streams.Key_Name =>
+                  if Reader.Key_Name = "id" then
+                     case Reader.Read_Next is
+                        when VSS.JSON.Streams.String_Value =>
+                           Item.Random_String := Reader.String_Value;
+                        when others =>
+                           raise Invalid_File;
+                     end case;
+                  elsif Reader.Key_Name = "actor" then
+                     Read_Actor (Reader, Item.Random_Int);
+                  end if;
+               when VSS.JSON.Streams.Start_Object =>
+                  Skip_Object (Reader);
+               when VSS.JSON.Streams.End_Object =>
+                  exit;
+               when others =>
+                  null;
+            end case;
+         end loop;
+      end Read_Object;
+
+      procedure Iterate_Objects
+         (Reader : in out VSS.JSON.Pull_Readers.JSON_Pull_Reader'Class)
+      is
+      begin
+         loop
+            case Reader.Read_Next is
+               --  Start of the next object
+               when VSS.JSON.Streams.Start_Object =>
+                  declare
+                     Another_Int : Long_Integer;
+                     Next_Object : Random_Object :=
+                        (Random_String => Empty_Virtual_String,
+                         Random_Int    => 0);
+                  begin
+                     Read_Object (Reader, Next_Object);
+                     Another_Int := Next_Object.Random_Int + 1;
+                  end;
+
+               when VSS.JSON.Streams.End_Array =>
+                  exit;
+
+               when others =>
+                  null;
+            end case;
+         end loop;
+      end Iterate_Objects;
+
+      Encoder : Virtual_String_Encoder;
+   begin
+      Encoder.Initialize ("utf-8", [Stateless => True, others => False]);
+      declare
+         Items  : constant Stream_Element_Vector := Encoder.Encode (JSON_VSS);
+         Stream : aliased
+            VSS.Text_Streams.Memory_UTF8_Input.Memory_UTF8_Input_Stream;
+         Reader : VSS.JSON.Pull_Readers.Simple.JSON_Simple_Pull_Reader;
+      begin
+         Stream.Set_Data (Items);
+         Reader.Set_Stream (Stream'Unchecked_Access);
+         End_Time := Clock;
+         Seconds := (End_Time - Start_Time);
+         Put_Line ("VSS.JSON read is " & Seconds'Image & " seconds");
+         --  Seek
+         Start_Time := Clock;
+         if Reader.Read_Next /= VSS.JSON.Streams.Start_Document then
+            raise Invalid_File;
+         end if;
+         loop
+            case Reader.Read_Next is
+               when VSS.JSON.Streams.Key_Name =>
+                  if Reader.Key_Name = True_Random_Key_VSS then
+                     --  Seek
+                     End_Time := Clock;
+                     Seconds := (End_Time - Start_Time);
+                     Put_Line ("VSS.JSON seek is " &
+                                 Seconds'Image & " seconds");
+                     -- Iterate
+                     Start_Time := Clock;
+                     Iterate_Objects (Reader);
+                     exit;
+                  end if;
+               when VSS.JSON.Streams.Start_Array =>
+                  Skip_Array (Reader);
+               when VSS.JSON.Streams.End_Document =>
+                  exit;
+               when others =>
+                  null;
+            end case;
+         end loop;
+      end;
+   end VSS_Test;
+   End_Time := Clock;
+   Seconds := (End_Time - Start_Time);
+   Put_Line ("VSS.JSON iter is " & Seconds'Image & " seconds");
 
 end Json_Benchmark;
